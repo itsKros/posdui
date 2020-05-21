fc9a47bd49deeb48d7a9e8113ec4f70b729e8637
Kros <kroshan197@gmail.com>
2020-05-21 23:38:36 +0530

    First

---
+++ dist/footer.php
@@ -0,0 +1,14 @@
+    <footer id="footer" class="bg-gray-900">
+        <div class="container mx-auto flex flex-col md:flex-row flex-wrap justify-center w-5/6 md:w-full py-10">
+            <div class="w-full md:w-3/4">
+                <p class="text-md  text-center md:text-left font-normal md:w-10/12 mb-5 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque illum nostrum omnis similique obcaecati. Sapiente, maiores ipsum dolorem exercitationem temporibus dicta laboriosam ducimus voluptates quam quo fugiat, eaque, iure doloremque nesciunt odio? Maiores pariatur illo nemo ipsa adipisci obcaecati. Aut.</p>
+            </div>
+            <div class="w-full md:w-1/4">
+                <p class="text-md  text-center md:text-left font-normal md:w-10/12 mb-5 text-white">© Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ullam!</p>
+            </div>
+        </div>
+    </footer>
+    
+    <!-- <script src="assets/js/script.js"></script> -->
+</body>
+</html>
---
+++ dist/index.php
@@ -0,0 +1,168 @@
+<?php include('header.php'); ?>
+    <!--Hero Starts-->
+    <section id="hero" class="border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-3xl md:text-4xl text-center font-bold md:w-7/12 mb-8 md:mb-12">Why Elmiron Users Are Holding The
+                Company Accountable And May Be
+                Entitled To Compensation In The Future
+                Due to Elmiron’s Connection To Vision
+                Problems
+            </h1>
+            <img class="w-full object-contain md:w-8/12 mb-10" src="assets/img/Elmiron-Advertorial-1.jpg" alt="">
+            <ul class="mb-4 list-style-disc list-inside">
+                <li class="px-4 py-2 text-xl md:text-2xl font-medium text-green-600"><span class="text-black">Have you taken Elmiron to treat bladder pain?</span></li>
+                <li class="px-4 py-2 text-xl md:text-2xl font-medium text-green-600"><span class="text-black">Have you been experiencing any vision problems?</span></li>
+                <li class="px-4 py-2 text-xl md:text-2xl font-medium text-green-600"><span class="text-black">Are you concerned the Elmiron might be damaging your eyes in a way you’re not aware of?</span></li>
+            </ul>
+            <h2 class="text-2xl text-center font-bold md:w-7/12">
+            If you answered yes to these questions, this article will likely be the
+            most important thing you read today.
+            </h2>
+        </div>
+    </section>
+    <!--Hero Ends-->
+
+    <!--Section 2 Starts-->
+    <section id="section-2" class="bg-grey-200 border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-2xl md:text-4xl text-center font-medium md:w-7/12 mb-8 md:mb-12 underline">Link Between Elmiron and Eye Damage</h1>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">Elmiron is a prescription drug used to treat what is commonly called “painful bladder syndrome,” and interstitial cystitis.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">It has been prescribed to treat osteoarthritis and is used as a coagulant as well.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">This drug has been widely prescribed for decades and has been used by hundreds of thousands of individuals.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">Many users of Elmiron take the drug for long periods of time, often using the drug for many years.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">Recently, a trend has been identified of Elmiron patients developing an eye condition called maculopathy.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">Maculopathy is a term that covers a number of conditions that affect the macula, which is part of the retina.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">This part of the eye controls highly sensitive vision, such as focusing and reading.</p>    
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">A study conducted by the American Academy of Ophthalmology showed that about a quarter of the patients observed with significant exposure to Elmiron showed signs of eye damage specifically to the retina.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">Those whose dosage was the highest also were among the most likely to exhibit damage to the macula.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-4">The doctors who performed this study believe that use of the drug is toxic to the macula and creating vision damage with prolonged exposure.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-8">There have also been recent reports indicating that in some cases, patients that do not report any vision symptoms have evidence of damage to the retina</p>
+            <ul class="mb-4 md:w-10/12 hidden">
+                <li class="px-4 py-2 box-shadow text-lg font-medium mb-4 rounded border-solid border border-gray-200"><span class="text-red-600">1.</span> The device doesn’t lock the lid properly (although it seems locked), and the user is able to take the lid off before the pressure has been released.</li>
+                <li class="px-4 py-2 box-shadow text-lg font-medium mb-4 rounded border-solid border border-gray-200"><span class="text-red-600">2.</span> The device indicates that all the pressure has been released, when in fact the release valve was clogged and the pressure was not fully released.</li>
+                <li class="px-4 py-2 box-shadow text-lg font-medium mb-4 rounded border-solid border border-gray-200"><span class="text-red-600">3.</span> The device explodes at some point during use.</li>
+            </ul>
+        </div>
+    </section>
+    <!--Section 2 Ends-->
+
+    <!--Section 3 Starts-->
+    <section id="section-3" class="border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-2xl md:text-4xl text-center font-medium md:w-7/12 mb-8 md:mb-12 underline">Identifying Maculopathy</h1>
+            <p class="text-md text-left text-xl font-normal md:w-10/12 mb-3">Maculopathy is a term that covers any disease or condition that damages the macula.</p>
+            <p class="text-md text-left text-xl font-normal md:w-10/12 mb-4">The type of this condition caused by Elmiron has a couple symptoms to watch out for:</p>
+            <p class="text-md text-left text-xl font-bold md:w-10/12 mb-3">1. Trouble focusing vision for tasks such as reading</p>
+            <p class="text-md text-left text-xl font-normal md:w-10/12 mb-4">- Damage to the macula can make it difficult for individuals to focus their vision to read.</p>
+            <p class="text-md text-left text-xl font-bold md:w-10/12 mb-3">2. Sensitivity to light or changes in lighting</p>
+            <p class="text-md text-left text-xl font-normal md:w-10/12 mb-4">- Maculopathy and damage to the macula can also make it difficult or painful for the eyes when adjusting to light or being exposed to bright light.</p>
+            <p class="text-md text-left text-xl font-bold md:w-10/12 mb-3">3. Dark spots on the retina, that can be observed during an eye examination.</p>
+            <p class="text-md text-left text-xl font-normal md:w-10/12 mb-4">- These spots are difficult to see without the proper medical equipment and we recommend any individual who believes they are at risk have an examination performed by a medical professional.</p>
+        </div>
+    </section>
+    <!--Section 3 Ends-->
+
+    <!--Section 4 Starts-->
+    <section id="section-4" class="bg-grey-200 border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-2xl md:text-4xl text-center font-medium md:w-7/12 mb-8 md:mb-12 underline">How to Take Action</h1>
+            <p class="text-left text-xl font-normal md:w-10/12 mb-4">If you or a loved one has taken Elmiron and experienced subsequent vision issues, the first action you want to take is getting your eyes and vision checked by a medical professional.</p>
+            <p class="text-left text-xl font-normal md:w-10/12 mb-4">Share any symptoms you have experienced with your doctor, especially if they match the list above. Also make sure to let your doctor know you have a history of using Elmiron.</p>
+            <p class="text-left text-xl font-normal md:w-10/12 mb-4">Recent recommendations for patients using Elmiron include:<br>
+                <span class="text-lg text-gray-700">
+                    A baseline examination with fundus photography, optical coherence tomography, and fundus autofluorescence
+                    imaging . These recommendations also include yearly examinations after more than 5 years of use of Elmiron.
+                </span>
+            </p>
+            <p class="text-left text-xl font-normal md:w-10/12 mb-4">Individuals who have already been diagnosed with maculopathy after taking Elmiron will want to read below on further action they can take.</p>
+        </div>
+    </section>
+    <!--Section 4 Ends-->
+
+    
+
+    <!--Section 5 Starts-->
+    <section id="section-5" class="border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-2xl md:text-4xl text-center font-medium md:w-7/12 mb-8 md:mb-12 underline">How We Can Help</h1>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">Johnson & Becker is helping a community of Elmiron patients hold the manufacturers responsible for the vision damage associated with long-term use of the drug.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">If you have taken Elmiron and would like to receive a FREE consultation, our team would be happy to review your case and discuss options with you.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">You are not alone if you have suffered the side-effects above and you may be entitled to significant financial compensation for your injuries.</p>
+            
+            <a class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-4 px-6 rounded-full mt-10" href="/survey">
+                Get A FREE Consultation
+            </a>
+        </div>
+    </section>
+    <!--Section 5 Ends-->
+
+    <!--Section 6 Starts-->
+    <section id="section-6" class="border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-5/6 py-20">
+            <h1 class="text-2xl md:text-4xl text-center font-medium md:w-7/12 mb-8 md:mb-12 underline">About Our Law Firm</h1>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">Johnson & Becker consists of exceptional lawyers who focus on handling nationwide cases that help make products safer for everyone.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">The experience and wealth of diversity at our law firm is extensive.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-2">We have played significant leadership roles in large settlements, decisions, and cases with regard to individual, consolidated multidistrict, and mass tort lawsuits in state and federal courts across the country.</p>
+            <p class="text-lg text-left font-normal md:w-10/12 mb-12">Suits we have filed against other medicine manufacturers have resulted in improvements in drug safety.</p>
+
+            <div class="container mx-auto flex flex-wrap flex-col items-center justify-between md:flex-row w-5/6 py-3">
+                <div class="w-full md:w-1/6 my-8">
+                    <img class="w-full object-cover object-left" src="assets/img/Pressure-Cooker-Advertorial-2.jpg" alt="">
+                </div>
+                <div class="w-full md:w-1/6 my-8">
+                    <img class="w-full object-contain object-left" src="assets/img/Pressure-Cooker-Advertorial-3.jpg" alt="">
+                </div>
+                <div class="w-full md:w-1/6 my-8">
+                    <img class="w-full object-cover object-left" src="assets/img/Pressure-Cooker-Advertorial-4.jpg" alt="">
+                </div>
+                <div class="w-full md:w-1/6 my-8">
+                    <img class="w-full object-contain object-left" src="assets/img/Pressure-Cooker-Advertorial-5.jpg" alt="">
+                </div>
+                <div class="w-full md:w-1/6 my-8">
+                    <img class="w-full object-cover object-left" src="assets/img/Pressure-Cooker-Advertorial-6.jpg" alt="">
+                </div>
+            </div>
+
+            <a class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-4 px-6 rounded-full mt-10" href="/survey">
+                Get A FREE Consultation
+            </a>
+        </div>
+    </section>
+    <!--Section 6 Ends-->
+
+    <!--Section 7 Starts-->
+    <section id="section-8" class="border-b">
+        <div class="container mx-auto flex flex-wrap flex-col items-center justify-center w-full md:w-5/6 py-20">
+            <div class="container mx-auto flex flex-wrap flex-col items-center justify-between w-5/6 py-3">
+                <!-- desktop -->
+                <div class="w-full my-8 hidden md:block">
+                    <img class="w-full object-cover object-left" src="assets/img/Pressure-Cooker-Advertorial-7.jpg" alt="">
+                </div>
+                <div class="w-full my-8 hidden md:block">
+                    <img class="w-full object-contain object-left" src="assets/img/Pressure-Cooker-Advertorial-8.jpg" alt="">
+                </div>
+                <div class="w-full my-8 hidden md:block">
+                    <img class="w-full object-cover object-left" src="assets/img/Pressure-Cooker-Advertorial-9.jpg" alt="">
+                </div>
+
+                <!-- mobile -->
+                <div class="w-full my-8 md:hidden block">
+                    <img class="w-full object-cover object-left" src="assets/img/mr-1.jpg" alt="">
+                </div>
+                <div class="w-full my-8 md:hidden block">
+                    <img class="w-full object-contain object-left" src="assets/img/mr-2.jpg" alt="">
+                </div>
+                <div class="w-full my-8 md:hidden block">
+                    <img class="w-full object-cover object-left" src="assets/img/mr-3.jpg" alt="">
+                </div>
+
+                
+            </div>
+
+            <a class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-4 px-6 rounded-full mt-10" href="/survey">
+                Get A FREE Consultation
+            </a>
+        </div>
+    </section>
+    <!--Section 7 Ends-->
+<?php include('footer.php'); ?>
